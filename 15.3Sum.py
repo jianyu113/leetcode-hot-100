2b192f4b54fec1d30e47b8d6fe118f64f64c7566
jianyu113 <jiayun042@gmail.com>
2025-12-05 23:51:35 -0500

    Add new file

---
+++ 15.3Sum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        nums.sort()             
+        n = len(nums)
+        res = set()             
+        for i in range(n):
+            target = -nums[i]
+            seen = set()
+            for j in range(i + 1, n):
+                needed = target - nums[j]
+                if needed in seen:
+                    res.add((nums[i], needed, nums[j]))
+                seen.add(nums[j])
+
+        return [list(t) for t in res]
